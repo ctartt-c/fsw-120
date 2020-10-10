4be83749c3a8dc8ba3c0c65c7419a937f1a1bcd8
ctartt-c <carlton.tartt@bryanuniversity.edu>
2020-10-10 02:51:46 -0500

    todo

---
+++ week1/todo/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input type="checkbox"/>
+      <p>my check box</p>
+
+      <input type="checkbox"/>
+      <p>my check box</p>
+
+      <input type="checkbox"/>
+      <p>my check box</p>
+    </div>
+  );
+}
+
+export default App;
